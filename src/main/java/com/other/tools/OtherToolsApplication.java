74af88d113cd03910557c8fe84c4de5132054521
apokochito <dpinto1@ucol.mx>
2020-01-08 13:37:49 -0600

    Base project

---
+++ src/main/java/com/other/tools/OtherToolsApplication.java
@@ -0,0 +1,13 @@
+package com.other.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtherToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtherToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/other/tools/OtherToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.other.tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtherToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
